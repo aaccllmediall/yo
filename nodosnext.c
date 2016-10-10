d8e61f97c851c0fd94b16a225097cca5776e271c
Eduardo Aliaga <aaaacccc_aaaacccc@hotmail.com>
2016-10-10 18:25:05 -0500

    :'v
    ................................................

---
+++ nodosnext.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int val;
+    struct node *next;
+};
+
+main()
+{
+    struct node *head, *middle, *tail;
+    head=malloc(sizeof(struct node));
+    middle=malloc(sizeof(struct node));
+    tail=malloc(sizeof(struct node));
+    head->val=1;
+    middle->val=2;
+    tail->val=3;
+    head->next=middle;
+    middle->next=tail;
+    tail->next=NULL;
+    struct node *t;
+    t=head;
+    while (t!=NULL){
+        printf("%d\n\n", t->val);
+        t=t->next;
+    }
+}
